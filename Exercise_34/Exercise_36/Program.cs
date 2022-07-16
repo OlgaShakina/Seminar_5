18ef7fa15b1c2dfa4feece5d8e7bc6b6b259a187
Olga <olga.buinova@gmail.com>
2022-07-16 23:57:29 +0300

    Новые задачи

---
+++ Exercise_34/Exercise_36/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+Console.WriteLine("Введите размер массива");
+int sum=0;
+int a=Convert.ToInt32(Console.ReadLine());
+int [] array= new int [a];
+for(int i =0; i<array.Length; i++){
+    array[i]=new Random().Next(-100,101);
+}
+Console.WriteLine("["+string.Join(", ",array)+"]");
+for(int i =1; i<array.Length; i+=2){
+    sum=sum+array[i];
+}
+Console.WriteLine("Сумма элементов на нечетных позициях = "+sum);
---
+++ Exercise_34/Exercise_38/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+Console.WriteLine("Введите размер массива");
+int max;
+int min;
+int dif;
+int a=Convert.ToInt32(Console.ReadLine());
+double[] array= new double [a];
+for(int i =0; i<array.Length; i++){
+    array[i]=new Random().Next(-100,101);
+}
+Console.WriteLine("["+string.Join(",",array)+"]");
